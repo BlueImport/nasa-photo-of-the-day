c47b615f926240d1b6d213875aa7704d97e07c06
Kris <kristian.claire.101@gmail.com>
2019-10-09 14:28:45 -0700

    adds background color

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
-import Styling from "../src/index.css";
-import ImgCard from "./Components/ImgCard";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Title from "./Components/ImgInfo";
+import Image from "./Components/ImgCard";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Nasa Image of the Day</h1>
-      <ImgCard />
+    <div className = "App">
+    <h1>Nasa Photo Of The Day</h1>
+
+    
+    <Image />
     </div>
   );
+  
 }
 
 export default App;
--- src/Components/ImgCard.js
+++ src/Components/ImgCard.js
@@ -15,7 +15,7 @@ const ImgCard = () => {
                 setTitle(response.data.title);
                 setDate(response.data.date);
                 setImage(response.data.url);
-                setExplanationText(response.data.explanation);
+                setExplanation(response.data.explanation);
                 console.log(response);
             });
     }, []);
